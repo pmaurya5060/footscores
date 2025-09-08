d8d9517ebedf9ae21c81f4992f97737912258595
pmaurya5060 <pmaurya5060@gamil.com>
2025-09-08 18:38:41 +0530

    resolving merge conflict

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,9 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+
 
 const Header = () => {
-  const [teamQuery, setTeamQuery] = useState("");
-  const navigate = useNavigate();
+  
 
-  const handleSearch = (e) => {
-    if (e.key === "Enter" && teamQuery.trim()) {
-      navigate(`/teams/${teamQuery}`);
-      setTeamQuery(""); 
-    }
-    else{
-      alert("Enter the team name correctly niggga");
-    }
-  };
+  
 
   return (
     <header className="navbar flex justify-between items-center px-5 py-[12px] rounded-[10px] text-white font-sans my-1.5 backdrop-blur-md top-0 fixed z-50 w-[90%] left-[5%] border border-white/10">
@@ -26,23 +16,7 @@ const Header = () => {
           <Link to="/" className="dropbtn">Matches</Link>
         </div>
 
-        <div className="dropdown">
-          <button className="dropbtn">Teams</button>
-          <div className="dropdown-content p-2">
-            <Link to="/teams/Portugal">Portugal</Link>
-            <Link to="/teams/Germany">Germany</Link>
-            <Link to="/teams/Brazil">Brazil</Link>
-
-            <input
-              type="text"
-              value={teamQuery}
-              onChange={(e) => setTeamQuery(e.target.value)}
-              onKeyDown={handleSearch}
-              placeholder="Search Team..."
-              className="mt-2 p-1 w-[140px] bg-white backdrop-blur-md text-black rounded"
-            />
-          </div>
-        </div>
+        
 
         <div className="dropdown">
           <Link to="/leagues" className="dropbtn">Competitions</Link>
