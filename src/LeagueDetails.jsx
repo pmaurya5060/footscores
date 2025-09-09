063fdfffe3fee91e240a7604cef3bd07e8d0640f
pmaurya5060 <pmaurya5060@gmail.com>
2025-09-09 14:59:27 +0530

    Added League details page with all season cards shown

--- src/LeagueDetails.jsx
+++ src/LeagueDetails.jsx
@@ -1,20 +1,31 @@
-import useLeagues from "./Hooks/useLeagues";
-import { useState } from "react";
+import useLeagues from './Hooks/useLeagues';
+// import {useState} from 'react';
 
 const LeagueDetails = () => {
     const LeagueData=useLeagues();
-    console.log(LeagueData);
+    // console.log("League details log ",LeagueData);
+    // console.log(LeagueData[0]?.league?.name);
+    // console.log(LeagueData[0].seasons);
+  
 
-    // const matchperpage=30 ;
-    // const start = (pageno-1) * matchperpage;
-    // const end = start + matchperpage;
-
-    // const currentMatches = matche.slice(start, end);
+  const alldata=LeagueData.map(l => {
 
 
-    
-  return (
-    <div>jhvgcvxdtfgyjhknmbbvghjnm v</div>
+    return(
+      <div 
+        key={l?.league?.id}
+        className="indvid_league bg-[#535C91]/20 backdrop-blur-sm p-4 rounded-2xl w-64 h-40 text-white flex flex-col items-center justify-center shadow-md hover:scale-105 transition-transform duration-500 cursor-pointer"
+        
+        >
+
+        <div className="name">{l?.league.name}</div>
+
+      </div>
+    )
+});
+
+  return(
+    <div className="lcontainer flex flex-wrap gap-6 justify-center p-6 my-25">{alldata}</div>
   )
 }
 
